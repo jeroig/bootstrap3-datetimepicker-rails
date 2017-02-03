b7e4670933b61f7b4d46a81591a5580b93c6824c
jeroig <jeroig@gmail.com>
2017-02-03 10:18:41 -0300

    Up to 800 tries I don't have problems.

--- vendor/assets/javascripts/bootstrap-datetimepicker.js
+++ vendor/assets/javascripts/bootstrap-datetimepicker.js
@@ -1660,8 +1660,8 @@
                 var tries = 0;
                 while (!isValid(date, 'd')) {
                     date.add(1, 'd');
-                    if (tries === 31) {
-                        throw 'Tried 31 times to find a valid date';
+                    if (tries === 800) {
+                        throw 'Tried 800 times to find a valid date';
                     }
                     tries++;
                 }
